5b360ff3650b0416f4e91c404936fed3fe5a1352
Joris Berthelot <admin@eexit.net>
2011-11-03 07:32:51 +0100

    updated vendors

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 use \Symfony\Component\ClassLoader\UniversalClassLoader;
-use \tests\Fs;
 
 require_once __DIR__ . '/../vendor/Symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 require_once __DIR__ . '/lib/atoum.phar';
