929c3eabd3cb7f2a04b5c5034631ee5c86979951
Joris Berthelot <admin@eexit.net>
2011-11-13 12:51:43 +0100

    Added photo class + tests

---
+++ lib/Smak/Portfolio/Photo.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Smak\Portfolio;
+
+class Photo extends \SplFileInfo
+{
+    /**
+     * Photo file type (IMAGE_XXX)
+     */
+    protected $_type;
+    
+    /**
+     * Photo width
+     */
+    protected $_width;
+    
+    /**
+     * Photo height
+     */
+    protected $_height;
+    
+    /**
+     * HTML photo size attribute
+     */
+    protected $_html_attr;
+    
+    /**
+     * Human reading photo size
+     */
+    protected $_size;
+    
+    /**
+     * Class constructor
+     * 
+     * @param string $file_name The file name (@see \SplFileInfo::__construct())
+     * @throws \RuntimeException
+     */
+    public function __construct($file_name)
+    {
+        parent::__construct($file_name);
+        
+        if (!extension_loaded('gd')) {
+            throw \RuntimeException('GD PHP extension not loaded!');
+        }
+        
+        list(
+            $this->_width,
+            $this->_height,
+            $this->_type,
+            $this->_html_attr
+        ) = getimagesize($this->getRealPath());
+        $this->_size = $this->_formatSize($this->getSize());
+    }
+    
+    /**
+     * Photo type getter
+     * 
+     * @return string
+     */
+    public function getPhotoType()
+    {
+        return $this->_type;
+    }
+    
+    /**
+     * Photo width getter
+     * 
+     * @return int
+     */
+    public function getWidth()
+    {
+        return $this->_width;
+    }
+    
+    /**
+     * Photo height getter
+     * 
+     * @return int
+     */
+    public function getHeight()
+    {
+        return $this->_height;
+    }
+    
+    /**
+     * HTML photo size attribute getter
+     * 
+     * @return string
+     */
+    public function getHtmlAttr()
+    {
+        return $this->_html_attr;
+    }
+    
+    /**
+     * Human photo size getter
+     * 
+     * @return string
+     */
+    public function getHRSize()
+    {
+        return $this->_size;
+    }
+    
+    /**
+     * Formats the file size for easy-human-reading
+     * 
+     * @param int $size The file size in bytes
+     * @throws \InvalidArgumentException
+     * @return string
+     */
+    private function _formatSize($size)
+    {
+        if (0 > $size) {
+            throw \InvalidArgumentException('File size cannot be lesser than 0 byte!');
+        }
+        
+        if ($size < 1e3) { // 0 octets > 9999 octets
+            $sext = 'b';
+        } elseif ($size >= 1e3 && $size < 1e6) { // 1 Ko > 9999 Ko
+            $size = round($size / 1e3);
+            $sext = 'Ko';
+        } elseif ($size >= 1e6 && $size < 1e7) { // 1 Mo > 9 Mo
+            $size = round($size / 1e6, 3);
+            $sext = 'Mo';
+        } elseif ($size >= 1e7 && $size < 1e8) { // 10 Mo > 99 Mo
+            $size = round($size / 1e6, 2);
+            $sext = 'Mo';
+        } elseif ($size >= 1e8 && $size < 1e9) { // 100 Mo > 999 Mo
+            $size = round($size / 1e6, 1);
+            $sext = 'Mo';
+        } elseif ($size >= 1e9) { // 1000 Mo et +
+            $size = round($size / 1e6);
+            $sext = 'Go';
+        }
+        
+        return sprintf('%d %s', $size, $sext);
+    }
+}
+
+?>
---
+++ tests/units/Smak/Portfolio/Photo.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Smak\Portfolio\tests\units;
+
+use mageekguy\atoum;
+use Smak\Portfolio;
+use tests\Fs;
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+class Photo extends atoum\test
+{
+    const FS_REL = '/../../../fs';
+    
+    const IMG_DIM = 10;
+    
+    const IMG_SIZE = 162;
+    
+    const IMG_TYPE = IMAGETYPE_JPEG;
+    
+    const IMG_DATA = <<< EOD
+/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEB
+AQEBAQEBAQEBAQECAgICAgICAgICAgMDAwMDAwMDAwP/wAALCAAKAAoBAREA/8QAFQABAQAAAAAA
+AAAAAAAAAAAAAAr/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/9oACAEBAAA/AL+AH//Z
+EOD;
+    
+    public function setUp()
+    {
+        $fs = new Fs(__DIR__ . self::FS_REL, $tree = $this->_fsTreeProvider());
+        $fs->build();
+        $this->assert->boolean($fs->isBuilt())->isTrue();
+        
+        $photo_test = array_shift($tree['Canon_450D']['Sandrine']);
+        file_put_contents(__DIR__ . self::FS_REL . '/Canon_450D/Sandrine/' . $photo_test,
+        base64_decode(self::IMG_DATA), LOCK_EX);
+    }
+    
+    public function beforeTestMethod($method)
+    {
+        $this->fs = new Fs(__DIR__ . self::FS_REL, $this->_fsTreeProvider());
+        $setRoot = new \SplFileInfo($this->fs->getRoot() . '/Canon_450D/Sandrine');
+        $this->set = new \Smak\Portfolio\Set($setRoot);
+        
+        $this->photo = $this->set->getPhotoById(0);
+        $this->assert->string($this->photo->getFilename())
+             ->isEqualTo('sample-1.jpg');
+    }
+    
+    public function testPhotoClassType()
+    {
+        foreach ($this->set->getPhotos() as $photo) {
+            $this->assert->object($photo)->isInstanceOf('\Smak\Portfolio\Photo');
+            $this->assert->object($photo)->isInstanceOf('\SplFileInfo');
+        }
+    }
+    
+    public function testPhotoHasRightSize()
+    {
+        $this->assert->integer($this->photo->getSize())
+             ->isEqualTo(self::IMG_SIZE);
+    }
+    
+    public function testGetHumanReadingSize()
+    {
+        $this->assert->string($this->photo->getHRSize())
+             ->isEqualTo(sprintf('%d %s', self::IMG_SIZE, 'b'));
+    }
+    
+    public function testPhotoComputedSize()
+    {
+        $this->assert->integer($this->photo->getWidth())
+             ->isEqualTo(self::IMG_DIM);
+        
+        $this->assert->integer($this->photo->getHeight())
+             ->isEqualTo(self::IMG_DIM);
+        
+        $this->assert->string($this->photo->getHtmlAttr())
+             ->isEqualTo(sprintf('width="%d" height="%d"', self::IMG_DIM, self::IMG_DIM));
+    }
+    
+    public function testPhotoType()
+    {
+        $this->assert->integer($this->photo->getPhotoType())
+             ->isEqualTo(self::IMG_TYPE);
+    }
+    
+    public function tearDown()
+    {
+        $fs = new Fs(__DIR__ . self::FS_REL, $this->_fsTreeProvider());
+        $fs->clear();
+    }
+    
+    private function _fsTreeProvider()
+    {
+        return array('Canon_450D'   => array(
+            'Sandrine'  => array(
+                'sample-1.jpg',
+                'sample-2.jpg',
+                'sandrine.twig'
+        )));
+    }
+}
